e45887f81e524cc2f58ec2d0919a94a78af44670
Daniel Neto <daniel@danielneto.dev.br>
2023-04-28 17:53:39 -0300

    change index.mjs

--- index.mjs
+++ index.mjs
@@ -1,10 +1,30 @@
-import { log } from './log.mjs'
+import { S3 } from '@aws-sdk/client-s3';
+
+import { log } from './log.mjs';
+
+const s3Client = new S3({
+  region: 'sa-east-1',
+});
 
 export const handler = async (event) => {
-  log('Running log: ' + JSON.stringify(event));
+  const record = event.Records[0];
+  const Bucket = record.s3.bucket.name;
+  const Key = record.s3.object.key;
 
-  return {
-    status: 200,
-    var: process.env.SOME_VAR,
-  };
+  const object = await s3Client.getObject({
+    Bucket,
+    Key,
+  });
+
+  const MEGA_BYTE = 1024 * 1024;
+
+  if (object.ContentLength > MEGA_BYTE) {
+    log('Very large object.');
+
+    return 'Very large object.';
+  }
+
+  log('Ok.');
+
+  return 'Ok.';
 }
